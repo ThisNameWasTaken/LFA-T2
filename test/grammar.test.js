25e4906eaa695a2637a17e8290764c0d8c1701c6
Adrian <adriandinca2008@yahoo.com>
2018-04-30 08:50:33 +0300

    Add another set of tests for the getWords method

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -6,10 +6,26 @@ const transitions = {
     'A': { 'c': ['A'] }
 };
 
+const transitions2 = {
+    'S': { 'a': ['A'], 'b': ['B'] },
+    'A': { 'a': ['S'] },
+    'B': { 'b': ['C'] },
+    'C': { 'b': ['D'] },
+    'D': { 'b': ['B'] }
+};
+
+const initialState = 'S';
+const finalStates = ['S', 'D'];
+
 describe('getWords', function () {
     it('should return the list of words known by a given grammar', function () {
         expect(new Grammar('S', 'A', transitions).getWords(2)).to.deep.equal(["ab", "bc"]);
         expect(new Grammar('S', 'A', transitions).getWords(3)).to.deep.equal(["aab", "abc", "bcc"]);
         expect(new Grammar('S', 'A', transitions).getWords(4)).to.deep.equal(["aaab", "aabc", "abcc", "bccc"]);
+        expect(new Grammar(initialState, finalStates, transitions2).getWords(2)).to.deep.equal(["aa"]);
+        expect(new Grammar(initialState, finalStates, transitions2).getWords(3)).to.deep.equal(["bbb"]);
+        expect(new Grammar(initialState, finalStates, transitions2).getWords(4)).to.deep.equal(["aaaa"]);
+        expect(new Grammar(initialState, finalStates, transitions2).getWords(5)).to.deep.equal(["aabbb"]);
+        expect(new Grammar(initialState, finalStates, transitions2).getWords(8)).to.deep.equal(["aaaaaaaa", "aabbbbbb"]);
     });
 });
