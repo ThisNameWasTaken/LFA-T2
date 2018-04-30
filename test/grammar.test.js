0e5d7699d3924cc9fcd1ad9524172711752d0c16
Adrian <adriandinca2008@yahoo.com>
2018-04-30 09:03:04 +0300

    Add a test for the Grammar's isWordValid method

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -6,10 +6,30 @@ const transitions = {
     'A': { 'c': ['A'] }
 };
 
+const transitions2 = {
+    'S': { 'a': ['A'], 'b': ['B'] },
+    'A': { 'a': ['S'] },
+    'B': { 'b': ['C'] },
+    'C': { 'b': ['D'] },
+    'D': { 'b': ['B'] }
+};
+
+const initialState = 'S';
+const finalStates = ['S', 'D'];
+
 describe('getWords', function () {
     it('should return the list of words known by a given grammar', function () {
         expect(new Grammar('S', 'A', transitions).getWords(2)).to.deep.equal(["ab", "bc"]);
         expect(new Grammar('S', 'A', transitions).getWords(3)).to.deep.equal(["aab", "abc", "bcc"]);
         expect(new Grammar('S', 'A', transitions).getWords(4)).to.deep.equal(["aaab", "aabc", "abcc", "bccc"]);
+    });
+});
+
+describe('isWordValid', function () {
+    it('should assert whether the gvien word is valid for the grammar or not', function () {
+        expect(new Grammar(initialState, finalStates, transitions2).isWordValid("aa")).to.equal(true);
+        expect(new Grammar(initialState, finalStates, transitions2).isWordValid("bbb")).to.equal(true);
+        expect(new Grammar(initialState, finalStates, transitions2).isWordValid("a")).to.equal(false);
+        expect(new Grammar(initialState, finalStates, transitions2).isWordValid("bbbb")).to.equal(false);
     });
 });
